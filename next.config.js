914738f89bfa710937569cb7efd89cd20ddf188b
ctilab-ava <89237300+BeautyKim@users.noreply.github.com>
2023-07-25 19:11:55 +0900

    typedRoutes 제거

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    typedRoutes: true,
     appDir: true,
   }
 }
--- src/app/dashboard/paage.tsx
+++ src/app/dashboard/paage.tsx
@@ -1,4 +1,5 @@
 export default function Dashboard() {
+  
   return (
     <div>
       <h1>대시보드</h1>
