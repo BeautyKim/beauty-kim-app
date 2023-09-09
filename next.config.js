a12a12f30c656c7ab798931a4268f6869f8e4d92
beautykim <89237300+BeautyKim@users.noreply.github.com>
2023-09-10 06:46:25 +0900

    config 옵션 제거

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: [
-      "www.notion.so",
-      'images.unsplash.com',
-      's3.us-west-2.amazonaws.com'
-    ],
-    format: ['image/png', 'image/webp', 'image/jpeg']
-  },
   experimental: {
     appDir: true,
   }
