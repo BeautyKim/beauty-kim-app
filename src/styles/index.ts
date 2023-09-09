1795cd0b0c8cbd4c67f0ce9142745283689b7d19
beautykim <89237300+BeautyKim@users.noreply.github.com>
2023-09-10 06:28:15 +0900

    Create index.ts

---
+++ src/styles/index.ts
@@ -0,0 +1,12 @@
+import "@/styles/board/boardPage.module.css";
+import "@/styles/board/codeBlock.module.css";
+import "@/styles/board/detailBoard.module.css";
+import "@/styles/board/galleryCard.module.css";
+import "@/styles/board/listCard.module.css";
+import "@/styles/board/newText.module.css";
+
+import "@/styles/home/footer.module.css";
+import "@/styles/home/header.module.css";
+import "@/styles/home/home.module.css";
+import "@/styles/home/searchBar.module.css";
+import "@/styles/home/title.module.css";
